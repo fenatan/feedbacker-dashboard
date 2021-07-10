75a3a999305e850fc94b4b7eef6c6b88d26d80d7
Felipe natan <felipenatan@novaandradina.org>
2021-07-10 12:37:21 -0400

    setup project

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
   },
   overrides: [
     {
