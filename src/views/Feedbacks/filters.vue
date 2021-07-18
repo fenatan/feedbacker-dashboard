340622ceded245268045f60b85fc97a1d4845dbe
Felipe natan <felipenatan@novaandradina.org>
2021-07-18 19:05:26 -0400

    improve css build size and configure netlify deploy

--- src/views/Feedbacks/filters.vue
+++ src/views/Feedbacks/filters.vue
@@ -11,12 +11,12 @@
         class="flex items-center justify-between px-4 py-1 rounded cursor-pointer"
       >
         <div class="flex items-center">
-          <span :class="`bg-${filter.color}`" class="inline-block w-2 h-2 mr-2 rounded-full" />
+          <span :class="filter.color.bg" class="inline-block w-2 h-2 mr-2 rounded-full" />
           {{ filter.label }}
         </div>
 
         <span
-          :class="filter.active ? `text-${filter.color}` : 'text-brand-graydark'"
+          :class="filter.active ? filter.color.text : 'text-brand-graydark'"
           class="font-bold"
           >{{ filter.amount }}</span
         >
@@ -39,10 +39,10 @@ const LABELS = {
 };
 
 const COLORS = {
-  all: 'brand-info',
-  issue: 'brand-danger',
-  idea: 'brand-warning',
-  other: 'brand-graydark',
+  all: { text: 'text-brand-info', bg: 'bg-brand-info' },
+  issue: { text: 'text-brand-danger', bg: 'bg-brand-danger' },
+  idea: { text: 'text-brand-warning', bg: 'bg-brand-warning' },
+  other: { text: 'text-brand-graydark', bg: 'bg-brand-graydark' },
 };
 
 function applyFiltersStructure(sumary) {
