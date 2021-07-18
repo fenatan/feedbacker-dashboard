df69c8ccc4b7a2a0bdc6378476182ed746a0be75
Felipe natan <felipenatan@novaandradina.org>
2021-07-18 17:56:36 -0400

    add ci unit and e2e

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   testMatch: ['**/*.spec.js'],
+  testPathIgnorePatterns: ['tests/e2e'],
   transform: {
     '^.+\\.vue$': 'vue-jest',
   },
